2cd8db7e8083fb5b4a0409cb0dbf7d2eb0883faa
ReDMallard <marco@titori.it>
2023-11-01 19:28:51 +0100

    starting point

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const autorized = [ "percollamarco8@gmail.com", "redmallardai@gmail.com", "percollamarco@virgilio.it"];
+
+const checkBtn = document.getElementById("getMail");
+checkBtn.addEventListener( "click" , function() {
+    const mailCheck = document.getElementById("mailInput").value ;
+    console.log(mailCheck)
+});
+
+
+
