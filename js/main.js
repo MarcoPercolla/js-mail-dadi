45cbae315d5f42495c7aeb836f63bdea75c6f703
ReDMallard <marco@titori.it>
2023-11-02 00:28:20 +0100

    accesso a zona separata e lancio dadi

--- js/main.js
+++ js/main.js
@@ -1,10 +1,41 @@
-const autorized = [ "percollamarco8@gmail.com", "redmallardai@gmail.com", "percollamarco@virgilio.it"];
-
+const autorized = [ "percollamarco8@gmail.com", "redmallardai@gmail.com", "percollamarco@virgilio.it" , "a"];
+const welcomeMessage = document.getElementById("container");
+const autorizedZone = document.getElementById("passValid")
+const dadiBtn = document.getElementById("lancia") ;
 const checkBtn = document.getElementById("getMail");
+const primoDado = document.getElementById("mioDado");
+const secondoDado = document.getElementById("againstDado");
+
+
+
+
 checkBtn.addEventListener( "click" , function() {
     const mailCheck = document.getElementById("mailInput").value ;
     console.log(mailCheck)
+
+    for (let i = 0; i < autorized.length; i++) {
+        const mailAutorized = autorized[i];
+        
+        if (mailAutorized == mailCheck) {
+            welcomeMessage.innerHTML = ` benvenuto :  ${mailCheck} `;
+            autorizedZone.classList.remove("d-none");
+            
+            break;
+            
+        }else  {
+            welcomeMessage.innerHTML = ` non puoi accedere :  ${mailCheck} `;
+        }
+        
+    }
 });
+
+
+dadiBtn.addEventListener("click" , function() {
+    primoDado.innerHTML = Math.floor(Math.random() * 6);
+    secondoDado.innerHTML = Math.floor(Math.random() * 6);
+
+    
+})
 
 
 
